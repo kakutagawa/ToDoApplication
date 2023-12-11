38a7e4e7c747789ff6a666fce336dfa0b6cb088c
kakutagawa <kaktgw0824@gmail.com>
2023-12-11 23:32:45 +0900

    Initial Commit

---
+++ ToDoApplication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoApplication
+//
+//  Created by 芥川浩平 on 2023/12/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoApplication/ToDoApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApplicationApp.swift
+//  ToDoApplication
+//
+//  Created by 芥川浩平 on 2023/12/11.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
