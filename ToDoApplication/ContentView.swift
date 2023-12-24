9e26d4aa99fa56dd91345e836c8b15c9389a391e
kakutagawa <kaktgw0824@gmail.com>
2023-12-25 00:36:38 +0900

    ToDoアプリ　いったん完成 q

--- ToDoApplication/ContentView.swift
+++ ToDoApplication/ContentView.swift
@@ -7,23 +7,54 @@
 
 import SwiftUI
 
+struct ToDoItem {
+    var isChecked: Bool
+    var task: String
+}
+
 struct ContentView: View {
-    @State var newItem: String = ""
+    @State var newTask: String = ""
+    @State var todoLists: [ToDoItem] = []
 
     var body: some View {
         VStack {
+            Text("ToDoリスト")
+                .font(.system(size: 30, weight: .bold, design: .default))
             HStack {
-                TextField("入力", text: $newItem)
+                TextField("入力", text: $newTask)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .frame(width: 300)
                 Button {
-
+                    todoLists.append(
+                        ToDoItem(isChecked: false, task: newTask)
+                    )
+                    newTask = ""
                 } label: {
                     Image(systemName: "plus.square.fill")
                         .resizable()
                         .scaledToFit()
                         .frame(width: 30, height: 30)
                 }
+            }
+            ForEach(todoLists.indices, id: \.self) { index in
+                HStack {
+                    Button {
+                        todoLists[index].isChecked.toggle()
+                    } label: {
+                        Image(systemName: todoLists[index].isChecked ? "checkmark.square" : "square")
+                        if todoLists[index].isChecked {
+                            Text(todoLists[index].task)
+                                .strikethrough()
+                                .foregroundStyle(.black)
+                        } else {
+                            Text(todoLists[index].task)
+                                .foregroundStyle(.black)
+                        }
+                    }
+                }
+                .padding(.top, 1)
+                .padding(.leading, 20)
+                .frame(maxWidth: .infinity, alignment: .leading)
             }
         }
         Spacer()
