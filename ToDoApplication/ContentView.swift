95f2a32a2a0aff3e235f2df8674a011b6a2450ae
kakutagawa <kaktgw0824@gmail.com>
2024-01-10 23:28:06 +0900

    Tab作成

--- ToDoApplication/ContentView.swift
+++ ToDoApplication/ContentView.swift
@@ -7,93 +7,22 @@
 
 import SwiftUI
 
-struct ToDoItem: Identifiable, Codable {
-    var id = UUID()
-    var isChecked: Bool
-    var task: String
-}
 
 struct ContentView: View {
-    @State var newTask: String = ""
-    @State var todoLists: [ToDoItem] = []
-
-    private let todoListKey = "todoListKey"
+    @State private var selection = 1
 
     var body: some View {
-        VStack {
-            Text("ToDoリスト")
-                .font(.system(size: 30, weight: .bold, design: .default))
-            HStack {
-                TextField("入力", text: $newTask)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .frame(width: 300)
-                Button {
-                    if !newTask.isEmpty {
-                        let newItem = ToDoItem(isChecked: false, task: newTask)
-                        todoLists.append(newItem)
-                        newTask = ""
-                    }
-                } label: {
-                    Image(systemName: "plus.square.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 30, height: 30)
+        TabView(selection: $selection) {
+            ToDoView()
+                .tabItem {
+                    Label("ToDo", systemImage: "checkmark.circle.fill")
                 }
-            }
-            ForEach(todoLists.indices, id: \.self) { index in
-                HStack {
-                    Button {
-                        todoLists[index].isChecked.toggle()
-                    } label: {
-                        Image(systemName: todoLists[index].isChecked ? "checkmark.square" : "square")
-                        if todoLists[index].isChecked {
-                            Text(todoLists[index].task)
-                                .strikethrough()
-                                .foregroundStyle(Color("TextColor"))
-                        } else {
-                            Text(todoLists[index].task)
-                                .foregroundStyle(Color("TextColor"))
-                        }
-                    }
-                    Spacer()
-                    Button {
-                        todoLists.remove(at: index)
-                    } label: {
-                        Image(systemName: "trash.fill")
-                            .padding(.trailing, 20)
-                    }
+                .tag(1)
+            NewsView()
+                .tabItem {
+                    Label("News", systemImage: "newspaper.fill")
                 }
-                .padding(.top, 1)
-                .padding(.leading, 20)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            }
-            Spacer()
-            Button {
-                todoLists.removeAll()
-            } label: {
-                Text("リセット")
-                    .font(.system(size: 20, weight: .bold, design: .default))
-                    .foregroundStyle(.red)
-            }
-        }
-        .onAppear {
-            if let savedToDoLists = UserDefaults.standard.data(forKey: todoListKey) {
-                do {
-                    let decoder = JSONDecoder()
-                    todoLists = try decoder.decode([ToDoItem].self, from: savedToDoLists)
-                } catch {
-                    print("Failed to decode TodoItems: \(error)")
-                }
-            }
-        }
-        .onDisappear {
-            do {
-                let encoder = JSONEncoder()
-                let encodedToDoLists = try encoder.encode(todoLists)
-                UserDefaults.standard.set(encodedToDoLists, forKey: todoListKey)
-            } catch {
-                print("Failed to encode ToDoItems: \(error)")
-            }
+                .tag(2)
         }
     }
 }
---
+++ ToDoApplication/ToDoView.swift
@@ -0,0 +1,103 @@
+//
+//  ToDoView.swift
+//  ToDoApplication
+//
+//  Created by 芥川浩平 on 2024/01/10.
+//
+
+import SwiftUI
+
+struct ToDoItem: Identifiable, Codable {
+    var id = UUID()
+    var isChecked: Bool
+    var task: String
+}
+
+struct ToDoView: View {
+    @State var newTask: String = ""
+    @State var todoLists: [ToDoItem] = []
+
+    private let todoListKey = "todoListKey"
+
+    var body: some View {
+        VStack {
+            Text("ToDoリスト")
+                .font(.system(size: 30, weight: .bold, design: .default))
+            HStack {
+                TextField("入力", text: $newTask)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 300)
+                Button {
+                    if !newTask.isEmpty {
+                        let newItem = ToDoItem(isChecked: false, task: newTask)
+                        todoLists.append(newItem)
+                        newTask = ""
+                    }
+                } label: {
+                    Image(systemName: "plus.square.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 30, height: 30)
+                }
+            }
+            ForEach(todoLists.indices, id: \.self) { index in
+                HStack {
+                    Button {
+                        todoLists[index].isChecked.toggle()
+                    } label: {
+                        Image(systemName: todoLists[index].isChecked ? "checkmark.square" : "square")
+                        if todoLists[index].isChecked {
+                            Text(todoLists[index].task)
+                                .strikethrough()
+                                .foregroundStyle(Color("TextColor"))
+                        } else {
+                            Text(todoLists[index].task)
+                                .foregroundStyle(Color("TextColor"))
+                        }
+                    }
+                    Spacer()
+                    Button {
+                        todoLists.remove(at: index)
+                    } label: {
+                        Image(systemName: "trash.fill")
+                            .padding(.trailing, 20)
+                    }
+                }
+                .padding(.top, 1)
+                .padding(.leading, 20)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            Spacer()
+            Button {
+                todoLists.removeAll()
+            } label: {
+                Text("リセット")
+                    .font(.system(size: 20, weight: .bold, design: .default))
+                    .foregroundStyle(.red)
+            }
+        }
+        .onAppear {
+            if let savedToDoLists = UserDefaults.standard.data(forKey: todoListKey) {
+                do {
+                    let decoder = JSONDecoder()
+                    todoLists = try decoder.decode([ToDoItem].self, from: savedToDoLists)
+                } catch {
+                    print("Failed to decode TodoItems: \(error)")
+                }
+            }
+        }
+        .onDisappear {
+            do {
+                let encoder = JSONEncoder()
+                let encodedToDoLists = try encoder.encode(todoLists)
+                UserDefaults.standard.set(encodedToDoLists, forKey: todoListKey)
+            } catch {
+                print("Failed to encode ToDoItems: \(error)")
+            }
+        }
+    }
+}
+
+#Preview {
+    ToDoView()
+}
