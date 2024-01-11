55072f6a40808dd9c36860649ea2b04552bffa3e
kakutagawa <kaktgw0824@gmail.com>
2024-01-12 00:36:22 +0900

    News FirstCommit

--- ToDoApplication/NewsView.swift
+++ ToDoApplication/NewsView.swift
@@ -7,9 +7,54 @@
 
 import SwiftUI
 
+struct NewsSource: Codable {
+    let status: String
+    let totalResults: Int
+    var articles: [Article]
+}
+
+struct Article: Codable {
+    var id: Int?
+    var author: String
+    var title: String
+    var description: String
+    var publishedAt: String
+}
+
+class GetNewsEventFetcher: ObservableObject {
+    @Published var articles: [Article] = []
+
+    func getNews() async {
+        guard let url = URL(string: "https://newsapi.org/v2/top-headlines?country=jp&apiKey=35dcdc32b4964d8ba8f209bb76addc6c") else {
+            return
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let decoder = JSONDecoder()
+            let searchedResult = try decoder.decode(NewsSource.self, from: data)
+            Task { @MainActor in
+                self.articles = searchedResult.articles
+            }
+        } catch {
+            print("エラー: \(error)")
+        }
+    }
+}
+
 struct NewsView: View {
+    @ObservedObject var store = GetNewsEventFetcher()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(self.store.articles, id: \.id) { res in
+            VStack(alignment: .leading) {
+                Text(res.author)
+                Text(res.title)
+                Text(res.description)
+                Text(res.publishedAt)
+            }
+            .padding()
+        }
     }
 }
 
